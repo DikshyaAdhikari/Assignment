757e4002eebb38a5b0f2fd51079cbf105511d8ec
DikshyaAdhikari <dikshya.adhikari143@gmail.com>
2020-07-09 00:34:49 -0500

    file added

---
+++ src/Encapsulation/Test.java
@@ -0,0 +1,11 @@
+package Encapsulation;
+
+public class Test {
+	public static void main(String[] args) {
+		Student s = new Student();
+		s.setName("Dikshya");
+		System.out.println(s.getName());
+		
+	}
+
+}
